90a59a5396244ea369972b538e58efab40f0f2e0
Aditya Patel <adityakdpatel@gmail.com>
2023-03-25 14:04:30 -0400

    added predict method

--- logistic_regression/algorithm.py
+++ logistic_regression/algorithm.py
@@ -121,7 +121,43 @@ def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
             print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
 
     return w, b, J_history  # return final w,b and J history for graphing
+def predict(X, w, b): 
+    """
+    Predict whether the label is 0 or 1 using learned logistic
+    regression parameters w
+    
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
 
+    Returns:
+      p : (ndarray (m,)) The predictions for X using a threshold at 0.5
+    """
+    # number of training examples
+    m, n = X.shape   
+    p = np.zeros(m)
+
+    ### START CODE HERE ### 
+    # Loop over each example
+    for i in range(m):   
+        z_wb = 0
+        # Loop over each feature
+        for j in range(n): 
+            # Add the corresponding term to z_wb
+            z_wb += X[i,j]*w[j]
+        
+        # Add bias term 
+        z_wb += b
+        
+        # Calculate the prediction for this example
+        f_wb = 1/(1+np.exp(-z_wb))
+
+        # Apply the threshold
+        p[i] = 1 if f_wb > 0.5 else 0
+        
+    ### END CODE HERE ### 
+    return p
 
 w_tmp = np.zeros_like(X_train[0])
 b_tmp = 0.
