02898b528433916050e69e8f5548d276287a9aba
Aditya Patel <adityakdpatel@gmail.com>
2025-03-01 18:02:32 -0500

    feat: minor changes

--- polynomial_regression/scikit_regression_algorithm.py
+++ polynomial_regression/scikit_regression_algorithm.py
@@ -5,7 +5,7 @@ np.set_printoptions(precision=2)
 
 
 def load_house_data():
-    data = np.loadtxt("data.txt", delimiter=',', skiprows=1)
+    data = np.loadtxt("home_prices_data.txt", delimiter=',', skiprows=1)
     X = data[:, :4]
     y = data[:, 4]
     print(X)
